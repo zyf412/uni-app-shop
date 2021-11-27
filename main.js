765da3b42d2ab813a39bd34e381d35dbcfe2ba38
张雨凡 <281671126@qq.com>
2021-11-27 16:59:43 +0800

    完成首页开发

--- main.js
+++ main.js
@@ -2,9 +2,29 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+uni.$showMsg = function(title = '数据请求失败！', duration = 1500) {
+	uni.showToast({
+		title,
+		duration,
+		icon: 'none'
+	})
+}
+// 请求拦截器
+$http.baseUrl = 'https://www.escook.cn'
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+  	title:'数据加载中...'
+  })
+}
+// 响应拦截器 
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
 
 Vue.config.productionTip = false
-
 App.mpType = 'app'
 
 const app = new Vue({
